c0ec496cbafc6eaba7dd30644a89afb8c589a540
lebatuananh <tuananh300496@gmail.com>
2020-09-03 15:14:03 +0700

    init

--- QHomeGroup/QHomeGroup.WebApplication/Services/IViewRenderService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace QHomeGroup.WebApplication.Services
-{
-    public interface IViewRenderService
-    {
-        Task<string> RenderToStringAsync(string viewName, object model);
-    }
-}
